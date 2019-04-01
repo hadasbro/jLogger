a73a35227533011514c85f863d1c0e649321e306
Slawek <slawomir.hadas@yahoo.com>
2019-04-01 20:52:37 +0100

    main

---
+++ jlogger/src/main/java/org/bitbucket/slawekhaa/jlogger/Main.java
@@ -0,0 +1,8 @@
+package org.bitbucket.slawekhaa.jlogger;
+
+@SuppressWarnings("unused")
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("main test");
+    }
+}
